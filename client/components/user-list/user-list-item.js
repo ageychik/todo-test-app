07eb7666f2efd8a7d6ae8fb38944587caf6ab2e3
Roma <forwm.ru@gmail.com>
2019-05-01 21:22:48 +0300

    project cleaning

--- client/components/user-list/user-list-item.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const UserListItem = props => (
-  <li><input type="checkbox" checked={props.isChecked} onClick={() => props.onToggle()} />{props.text}
-  </li>
-);
-export default UserListItem;
